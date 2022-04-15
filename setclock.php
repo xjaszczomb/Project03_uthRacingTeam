f35e98b010a6980a750e7964964a4b2300fd8986
xjaszczomb <kovvalska@onet.pl>
2022-04-15 19:17:52 +0200

    Adding reset POST event

--- setclock.php
+++ setclock.php
@@ -1,17 +1,40 @@
 <?php
 if(isset($_POST['setclock'])) {
+    if($_POST['minutes']!="") {
+        require_once "connect.php";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        //echo "Connected successfully"
+
+        $minutes = $_POST['minutes'];
+        $time = time()+$minutes*60;
+
+        $conn->query("UPDATE `minutes` SET `minutes`=$minutes WHERE `minutes` IS NOT NULL");
+        $conn->query("UPDATE `settime` SET `time`=$time WHERE `time` IS NOT NULL");
+
+        $conn->close();
+    }
+    header('Location: index.php');
+}
+
+if(isset($_POST['reset'])) {
     require_once "connect.php";
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
-    //echo "Connected successfully";
-    $minutes = $_POST['minutes'];
-    $time = time()+$minutes*60;
+    //echo "Connected successfully"
+    
+    $resultMinutes = $conn->query("SELECT * FROM `minutes` LIMIT 1");
 
-    $query = "UPDATE `settime` SET `settime`=$time WHERE `settime` IS NOT NULL";
-    $conn->query($query);
+    $minutes = $resultMinutes->fetch_assoc();
+    $time = time()+$minutes['minutes']*60;
+
+    $conn->query("UPDATE `settime` SET `time`=$time WHERE `time` IS NOT NULL");
 
     $conn->close();
     header('Location: index.php');
@@ -25,10 +48,10 @@ if(isset($_POST['stopclock'])) {
     die("Connection failed: " . $conn->connect_error);
     }
 
-    $resultClock = $conn->query("SELECT * FROM  startstop");
-    $stoppedClock = mysqli_fetch_row($resultClock);
+    $resultClock = $conn->query("SELECT * FROM  startstop LIMIT 1");
+    $stoppedClock = $resultClock->fetch_assoc();
 
-    if(!$stoppedClock[0]) {
+    if(!$stoppedClock['stopped']) {
         $time = time();
         $conn->query("UPDATE `startstop` SET `stopped`=1 WHERE `stopped` IS NOT NULL");
         $conn->query("UPDATE `stopdate` SET `time`=$time WHERE `time` IS NOT NULL");
@@ -46,21 +69,21 @@ if(isset($_POST['startclock'])) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $resultClock = $conn->query("SELECT * FROM  startstop");
-    $stoppedClock = mysqli_fetch_row($resultClock);
+    $resultClock = $conn->query("SELECT * FROM  startstop LIMIT 1");
+    $stoppedClock = $resultClock->fetch_assoc();
 
-    if($stoppedClock[0]) {
-        $resultSet = $conn->query("SELECT * FROM settime");
-        $resultStopDate = $conn->query("SELECT * FROM  stopdate");
+    if($stoppedClock['stopped']) {
+        $resultSet = $conn->query("SELECT * FROM settime LIMIT 1");
+        $resultStopDate = $conn->query("SELECT * FROM  stopdate LIMIT 1");
 
         $time = time();
-        $setTime = mysqli_fetch_row($resultSet);
-        $stopDate = mysqli_fetch_row($resultStopDate);
+        $setTime = $resultSet->fetch_assoc();
+        $stopDate = $resultStopDate->fetch_assoc();
 
-        $setTime[0]=$setTime[0]+($time-$stopDate[0]);
+        $setTime['time']=$setTime['time']+($time-$stopDate['time']);
 
         $conn->query("UPDATE `startstop` SET `stopped`=0 WHERE `stopped` IS NOT NULL");
-        $conn->query("UPDATE `settime` SET `settime`=$setTime[0] WHERE `settime` IS NOT NULL");
+        $conn->query('UPDATE `settime` SET `time`='.$setTime['time'].' WHERE `time` IS NOT NULL');
 
     }
     $conn->close();
