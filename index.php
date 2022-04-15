72a1bcf9bdd27167f46f27f3038e4e21a0acc29e
xjaszczomb <kovvalska@onet.pl>
2022-04-15 19:58:43 +0200

    Basic plan of html blocks

--- index.php
+++ index.php
@@ -9,9 +9,10 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UTH Racing Team</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="result">00:00</div>
+    <!-- <div id="result">00:00</div>
 
     <script>
         const inter = setInterval(Timer,1000);
@@ -21,7 +22,42 @@ session_start();
             xmlhttp.send(null);
             document.getElementById("result").innerHTML=xmlhttp.responseText;
         } 
-    </script>
+    </script> -->
 
+    <main>
+        <nav>
+            <button>Track Day</button>
+            <div>
+
+            </div>
+            <button>Racing Team</button>
+        </nav>
+        <article>
+            <section>
+                <div>
+                    <img src="" alt="logo uth racing team">
+                </div>
+                <header>
+                    <div><hr></div>
+                    <h2>Uth racing team</h2>
+                    <div>
+                        <div>Motoryzacja</div>
+                        <div>Track Days</div>
+                        <div>Zabawa</div>
+                    </div>
+                </header>
+                <div>
+                    <img src="" alt="samochód">
+                </div>
+            </section>
+        </article>
+        <nav>
+            <button>Zaloguj</button>
+            <div>
+                
+            </div>
+            <button>Sponsorzy</button>
+        </nav>
+    </main>
 </body>
 </html>
