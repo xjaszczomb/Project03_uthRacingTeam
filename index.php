3f7968669a66d25109085318cd7e63aca46de639
xjaszczomb <kovvalska@onet.pl>
2022-04-22 15:22:06 +0200

    create table and fix navbar position

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UTH Racing Team</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery.min.js"></script>
 </head>
 <body>
     <!-- <div id="result">00:00</div>
@@ -24,29 +25,33 @@ session_start();
         } 
     </script> -->
 
-    <main class="flex">
-        <nav class="fixed" data-position="left" data-version="desktop">
-            <button class="">Track Day</button>
-            <div>
+    <nav class="fixed" data-position="left" data-version="desktop">
+        <button class="btn-dark round">Track Day</button>
+        <div>
 
-            </div>
-            <button>Racing Team</button>
-        </nav>
-        <nav class="sm-nav top fixed flex" data-version="mobile">
-            <button class="btn-dark round">Track Day</button>
-            <button class="btn-light round">Zaloguj</button>
-        </nav>
-        <nav class="sm-nav bottom fixed flex" data-version="mobile">
-            <button class="btn-white">Racing Team</button>
-            <button class="btn-white">Sponsorzy</button>
-        </nav>
+        </div>
+        <button class="btn-white">Racing Team</button>
+    </nav>
+
+    <nav class="sm-nav top sticky flex" data-version="mobile">
+        <button class="btn-dark round">Track Day</button>
+        <button class="btn-light round">Zaloguj</button>
+    </nav>
+
+    <nav class="sm-nav bottom fixed flex" data-version="mobile">
+        <button class="btn-white">Racing Team</button>
+        <button class="btn-white">Sponsorzy</button>
+    </nav>
+    <main class="flex main">
         <article>
+            <div class="logo flex">
+                <img src="img/logo.png" alt="logo uth racing team">
+            </div>
             <section>
-                <div>
-                    <img src="img/logo.png" alt="logo uth racing team">
-                </div>
                 <header>
-                    <div><hr></div>
+                    <div class="line">
+                        <hr>
+                    </div>
                     <div class="title-holder">
                         <h2 class="title">uth</h2>
                         <h2 class="title">racing</h2>
@@ -58,13 +63,15 @@ session_start();
                         <div class="tag">Zabawa</div>
                     </div>
                 </header>
-                <div>
-                    <!-- <img src="img/car.png" alt="samochód"> -->
+                <div class="img-xl">
+                    <img src="img/car.png" alt="samochód">
                 </div>
             </section>
             <section>
                 <header>
-                    <div><hr></div>
+                    <div class="line">
+                        <hr>
+                    </div>
                     <div class="title-holder">
                         <h2 class="title">Track Day</h2>
                     </div>
@@ -74,15 +81,46 @@ session_start();
                         <div class="tag">Trening</div>
                     </div>
                 </header>
+                <table class="trackday-table">
+                    <thead>
+                        <tr>
+                            <th>Imię</th>
+                            <th>Nazwisko</th>
+                            <th>Auto</th>
+                            <th class="group">Gr.</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Martyna</td>
+                            <td>Jastrzębska</td>
+                            <td>Honda Civic</td>
+                            <td class="group">1</td>
+                        </tr>
+                        <tr>
+                            <td>Artur</td>
+                            <td>Orkwiszewski</td>
+                            <td>Subaru Impreza</td>
+                            <td class="group">1</td>
+                        </tr>
+                        <tr>
+                            <td>abcdefghijklmnopr</td>
+                            <td>SRATATATATAATaaaaaaa</td>
+                            <td>Subaru Imprezaaaaaaaaaaa</td>
+                            <td class="group">1</td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
         </article>
-        <nav class="fixed" data-position="right" data-version="desktop">
-            <button>Zaloguj</button>
-            <div>
-                
-            </div>
-            <button>Sponsorzy</button>
-        </nav>
     </main>
+    <nav class="fixed" data-position="right" data-version="desktop">
+        <button>Zaloguj</button>
+        <div>
+            
+        </div>
+        <button>Sponsorzy</button>
+    </nav>
+    <script src="js/script.js"></script>
 </body>
 </html>
