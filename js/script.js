9270e33aae4de43080da3715939464237883f5c4
xjaszczomb <kovvalska@onet.pl>
2022-04-22 18:20:33 +0200

    script for dropdown content and clock

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const dropdownContent= document.querySelector(".dropdown-content");
+
+$(document).ready(()=> {
+    $(".dropdown").click(function(){
+        $(".dropdown-content").animate({
+            height: 'toggle'
+        });
+
+       // $(".dropdown").find("svg").css("transform", "rotate(180deg)")
+    
+        let visible = dropdownContent.getAttribute("data-visible")
+        visible==="true" ? dropdownContent.setAttribute("data-visible",false) : dropdownContent.setAttribute("data-visible",true)
+    });
+})
+
+
+const inter = setInterval(Timer,1000);
+    function Timer(){
+        let xmlhttp= new XMLHttpRequest();
+        xmlhttp.open("GET","clock.php",false);
+        xmlhttp.send(null);
+        document.getElementById("result").innerHTML=xmlhttp.responseText;
+    }
