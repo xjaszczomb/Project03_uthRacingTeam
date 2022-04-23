7105fbd1efb97a64b9d63b33cbfd0a684f361cb2
xjaszczomb <kovvalska@onet.pl>
2022-04-23 23:56:43 +0200

    styling login.php and organising

--- login.php
+++ login.php
@@ -13,6 +13,26 @@ session_start();
     <script src="js/jquery.min.js"></script>
 </head>
 <body>
-    <script src="js/script.js"></script>
+    <nav class="sm-nav top fixed flex" data-version="mobile">
+        <a href="index.php"><button class="btn-light round">Powrót</button></a>
+    </nav>
+    <main class="window-center flex">
+        <form action="" method="POST">
+            <div class="form-items-holder flex">
+                <div class="form-items flex">
+                    <div class="info login-info">
+                        <p>Logowanie</p>
+                    </div>
+                    <div class="form-inputs flex">
+                        <input type="text" name="login" placeholder="Login">
+                        <input type="text" name="password" placeholder="Hasło">
+                    </div>
+                    <div class="form-btns flex end" data-typehold="button">
+                        <button class="btn-dark round" type="submit" name="adduser">Zaloguj</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
 </body>
 </html>
