38dc24ab1ace2abee33d54723301fae7294a973f
xjaszczomb <kovvalska@onet.pl>
2022-04-23 21:56:47 +0200

    init adduser

---
+++ adduser.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTH Racing Team</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+        
+        <nav class="nav-admin bottom fixed flex">
+            <div class="nav-admin-list flex">
+                <div class="nav-admin-item flex round">
+                    <img src="icons/clock.svg" alt="adduser">
+                </div>
+                <div class="nav-admin-item flex round">
+                    <img src="icons/users.svg" alt="adduser">
+                </div>
+                <div class="nav-admin-item flex round">
+                    <img src="icons/user-plus.svg" alt="adduser">
+                </div>
+            </div>
+        </nav>
+    
+
+    <script src="js/script.js"></script>
+</body>
+</html>
