29037879f2bf8731a590ab86151e7adb6af7e002
xjaszczomb <kovvalska@onet.pl>
2022-04-23 23:23:07 +0200

    change class name

--- admin.php
+++ admin.php
@@ -13,46 +13,44 @@ session_start();
     <script src="js/jquery.min.js"></script>
 </head>
 <body>
-        <div class="clock-settings flex">
-            <div class="clock-holder flex">
-                <div class="clock round" id="result">00:00</div>
-            </div>
-            <form class="clock-form" action="setclock.php" method="POST">
-                <div class="form-items-holder flex">
-                    <div class="form-items flex">
-                        <div class="form-btns flex">
-                            <input class="form-btn" type="submit" name="setclock" value="ustaw">
-                            <input class="form-btn" type="submit" name="stopclock" value="stop">
-                            <input class="form-btn" type="submit" name="startclock" value="start">
-                            <input class="form-btn" type="submit" name="reset" value="reset">
-                        </div>
-                        <input type="number" name="minutes">
-                        <p>Aktualne ustawienie: <?php require_once "php/currentsettime.php";?></p>
-                    </div>
-                </div>
-            </form>
+    <main class="window-center flex">
+        <div class="clock-holder flex">
+            <div class="clock round" id="result">00:00</div>
         </div>
-        <nav class="nav-admin bottom fixed flex">
-            <div class="nav-admin-list flex">
-                <a href="admin.php">
-                    <div class="nav-admin-item flex round">
-                        <img src="icons/clock.svg" alt="adduser">
+        <form class="clock-form" action="setclock.php" method="POST">
+            <div class="form-items-holder flex">
+                <div class="form-items flex">
+                    <div class="form-btns flex">
+                        <input class="form-btn" type="submit" name="setclock" value="ustaw">
+                        <input class="form-btn" type="submit" name="stopclock" value="stop">
+                        <input class="form-btn" type="submit" name="startclock" value="start">
+                        <input class="form-btn" type="submit" name="reset" value="reset">
                     </div>
-                </a>
-                <a href="users.php">
-                    <div class="nav-admin-item flex round">
-                        <img src="icons/users.svg" alt="adduser">
-                    </div>
-                </a>
-                <a href="adduser.php">
-                    <div class="nav-admin-item flex round">
-                        <img src="icons/user-plus.svg" alt="adduser">
-                    </div>
-                </a>
+                    <input type="number" name="minutes">
+                    <p>Aktualne ustawienie: <?php require_once "php/currentsettime.php";?></p>
+                </div>
             </div>
-        </nav>
-    
-
+        </form>
+    </main>
+    <nav class="nav-admin bottom fixed flex">
+        <div class="nav-admin-list flex">
+            <a href="admin.php">
+                <div class="nav-admin-item flex round">
+                    <img src="icons/clock.svg" alt="adduser">
+                </div>
+            </a>
+            <a href="users.php">
+                <div class="nav-admin-item flex round">
+                    <img src="icons/users.svg" alt="adduser">
+                </div>
+            </a>
+            <a href="adduser.php">
+                <div class="nav-admin-item flex round">
+                    <img src="icons/user-plus.svg" alt="adduser">
+                </div>
+            </a>
+        </div>
+    </nav>
     <script src="js/script.js"></script>
 </body>
 </html>
