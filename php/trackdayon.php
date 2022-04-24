8630fe3c1e2f2f4634da6a7dedcfa8347bc97a93
xjaszczomb <kovvalska@onet.pl>
2022-04-24 23:47:45 +0200

    Counting groups and users, temporary removing slideup

--- php/trackdayon.php
+++ php/trackdayon.php
@@ -6,15 +6,28 @@
 <div class="table-info flex">
     <div class="info">
         <p>Uczestnicy</p>
-        <span>Grupy: </span>
-        <span>Uczestnicy: </span>
+        <?php
+        require "connect.php";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $resultUsers = $conn->query("SELECT COUNT(id) FROM users");
+        $resultGroups = $conn->query("SELECT COUNT(DISTINCT `group`) FROM users");
+        $users = $resultUsers->fetch_row();
+        $groups = $resultGroups->fetch_row();
+        echo "<span>Grupy: ".$groups[0]."</span>
+        <span>Uczestnicy: ".$users[0]."</span>";
+        $conn->close();
+        ?>
     </div>
     <div class="dropdown-menu">
-        <button class="dropdown flex">Zwiń
+        <!-- <button class="dropdown flex">Zwiń
             <svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 4.5L4 1.5L1 4.5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
-        </button>
+        </button> -->
     </div>
 </div>
 <div class="dropdown-content" data-visible="true">
