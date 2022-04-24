d08b0f834708703bf9431e91b49201ae8b93835b
xjaszczomb <kovvalska@onet.pl>
2022-04-24 22:06:11 +0200

    init login backend

---
+++ php/logincheck.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if(isset($_POST['logging'])) {
+    if($_POST['login'] != "" && $_POST['password'] != "") {
+        require_once "connect.php";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        //echo "Connected successfully"
+
+        $login = $_POST['login'];
+        $passwd = $_POST['password'];
+        
+        $check = $conn->query("SELECT * FROM `admin` WHERE `login`='$login' AND `password`='$passwd'");
+        $countnums = $check->num_rows;
+        if($countnums == 1) {
+            $_SESSION['logged']=true;
+            header('Location: ../admin.php');
+        } else header('Location: ../login.php');
+
+        $conn->close();
+    }
+    header('Location: ../login.php');
+}
+if(isset($_POST['logout'])) {
+    
+    $_SESSION['logged']=false;
+    header('Location: ../index.php');
+}
+
+?>
