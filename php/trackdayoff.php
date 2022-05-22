fe03d30a660a5dfd00602c50c8b0d9f2b9d52b01
xjaszczomb <kovvalska@onet.pl>
2022-05-22 19:59:00 +0200

    w.i.p

--- php/trackdayoff.php
+++ php/trackdayoff.php
@@ -1,10 +1,10 @@
 <div class="trackday-banner">
     <div class="track-date">
         <span>Najbliższy event</span>
-        <p class="date-info">25 kwietnia 2022</p>
+        <p class="date-info">23 maja 2022</p>
         <p class="place-info">tor słomczyn</p>
         <p class="other-info">8:00-16:00</p>
-        <p class="other-info">drift</p>
+        <p class="other-info">speed/drift</p>
     </div>
     <div class="img-xl">
         <img src="img/trackday.png" alt="samochód">
