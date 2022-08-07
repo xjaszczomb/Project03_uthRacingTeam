d4741eed24c7cd088734aae3bac817249bf784b9
xjaszczomb <kovvalska@onet.pl>
2022-08-07 16:54:38 +0200

    event database communication

--- php/trackdayoff.php
+++ php/trackdayoff.php
@@ -1,10 +1,34 @@
 <div class="trackday-banner">
     <div class="track-date">
-        <span>Najbliższy event</span>
-        <p class="date-info">23 maja 2022</p>
-        <p class="place-info">tor słomczyn</p>
-        <p class="other-info">8:00-16:00</p>
-        <p class="other-info">speed/drift</p>
+        <span>Najbliższe eventy</span>
+
+        <?php
+            require "connect.php";
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+        
+            $resultEvents = $conn->query("SELECT * FROM events  WHERE `date`>=CURRENT_DATE() Order BY `date`");
+            $events = $resultEvents->fetch_assoc();
+            $i=0;
+
+            while($events && $i<2) {
+                $date = date_create($events['date']);
+
+                echo "<p class='date-info'>".date_format($date, 'd-m-Y')."</p>
+                <p class='place-info'>".$events['place']."</p>
+                <p class='other-info'>".$events['hourstart']."-".$events['hourend']."</p>
+                <p class='other-info'>".$events['type']."</p>";
+
+                $events = $resultEvents->fetch_assoc();
+                $i++;
+            }
+            
+            $conn->close();
+        ?>
+
     </div>
     <div class="img-xl">
         <img src="img/trackday.png" alt="samochód">
